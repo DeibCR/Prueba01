567f3f9c8b84ed65c7688319a0003d6b0faa8835
DeibCR <117248011+DeibCR@users.noreply.github.com>
2022-11-25 11:27:33 -0600

    Project uploaded

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php 
+    namespace Medoo;
+    require 'Medoo.php';
+
+    if(!isset($database)){
+        $database = new Medoo([
+            // [required]
+            'type' => 'mysql',
+            'host' => 'db4free.net:3306',
+            'database' => 'fb_recipes',
+            'username' => 'fb_admin',
+            'password' => 'foodbook_root' 
+        ]);
+    }
+?>
